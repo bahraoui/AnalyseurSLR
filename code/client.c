17951d549a51bcd623ae29024f00000d50d475bb
Bahraoui Marwane <bahraoui.marwane@gmail.com>
2021-12-20 18:43:34 +0100

    j'ai pas compris

--- code/client.c
+++ code/client.c
@@ -4,7 +4,9 @@
 int main(int argc, char const *argv[])
 {
     /****************************
-        Verification du bon nombre d'arguments [Bon nombre = 2]
+     * 
+     *  Verification du bon nombre d'arguments [Bon nombre = 2]
+     * 
     ****************************/
 
     if (argc < 3) // nombre d'arguments trop petit
@@ -28,31 +30,57 @@ int main(int argc, char const *argv[])
     strcpy(fichier, argv[1]);
 
     FILE *file = fopen(fichier, "r+");
-    printf("Validation du fichier en cours ...\n");
+    file_read fichierLu;
+    printf("Lecture du fichier en cours ...\r");
     if (file != NULL) // Fichier existant
     {
-        printf("--> Le fichier %s existe\n", fichier);
-        file_read marwane = read_file(fichier);
-        printf("--> Le fichier %s est bien lu\n", fichier);
+        printf("Lecture du fichier en cours ... \xE2\x9C\x94\n");
+        printf("Validation du fichier en cours ...\r");
+        fichierLu = read_file(fichier);
+        printf("Validation du fichier en cours ... \xE2\x9C\x94\n");
         fclose(file);
     }
     else // Fichier inexistant ou impossible d'acces
     {
-        fprintf(stderr, "--> Impossible d'ouvrir le fichier %s\n", fichier);
+        fprintf(stderr, "Impossible d'ouvrir le fichier %s\n", fichier);
         exit(EXIT_FAILURE);
     }
 
     // Lecture du mot en entree [Second Argument] :
-    printf("Validation du mot en cours ...\n");
+    printf("Validation du mot en cours ...\r");
     char *mot = (char *)malloc(strlen(argv[2]) + 1);
     strcpy(mot, argv[2]);
-    printf("--> Le mot est valide ");
+    printf("Validation du mot en cours ... \xE2\x9C\x94\n");
 
     // affichage des valeurs donnees en entree et verifiees :
-    printf("\n\nFichier : %s\nMot : %s\n", fichier, mot);
+    printf("\nFichier : %s\nMot : %s\n\n##############################\nDebut Algo SLR\n##############################\n\n", fichier, mot);
 
-    /****************************
-        FIN de Verification du bon nombre d'arguments [Bon nombre = 2]
-    ****************************/
+    /*##########################
+     * 
+     * FIN de Verification du bon nombre d'arguments [Bon nombre = 2]
+     * 
+    ##########################*/
+
+    /******
+    * oui
+    ******/
+    printf("Flot | Pile\n");
+    char c1 = mot[0];
+    char* flot = (char*)calloc(strlen(mot),sizeof(char));
+    char* pile = (char*)calloc(strlen(mot)*2,sizeof(char));
+
+    for (size_t i = 0; i != fichierLu.t.nblines; i++)
+    {
+    printf("fichierLu.t.trans : %d\n",fichierLu.t.trans[256*i]);
+    }
+
+    /*
+    unsigned short taille_mot = strlen(mot);
+    for (size_t i = 0; i < taille_mot; i++)
+    {
+        printf("i:%d,a:%c\n",i,mot[i]);
+    }*/
+    printf("accept\n");
+   
     return 0;
 }
